bc3fe4a490964a5bb05d3893c8e872579d2891d1
Chunxian Zhang <1836891291@qq.com>
2020-10-19 17:02:07 +0800

    finish 2.2.3 2.cpp

--- 第二章 线性表/综合练习题/2.2.3/1.cpp
+++ 第二章 线性表/综合练习题/2.2.3/1.cpp
@@ -53,3 +53,61 @@ int fun1(Sqlist &L)
 }
 
 // 和标准答案差不多
+
+// 动态顺序表
+
+#define INITSIZE 10
+
+typedef struct
+{
+    int *data;                      // 指示动态分配数组的指针
+    int MaxSize;                    // 顺序表的最大容量
+    int length;                     // 顺序表的当前长度
+}DSqlist;
+
+bool DInitList(DSqlist &L)
+{
+    L.data = (int *)malloc(INITSIZE * sizeof(DSqlist));
+
+    L.MaxSize = INITSIZE;
+
+    L.length = 0;
+}
+
+bool Dfun(DSqlist &L, int &mini_element)
+{
+    if (L.length == 0 || L.length == 1)
+    {
+        return false;
+    }
+
+    mini_element = L.data[0];
+    int mini_index = 0;
+
+    for (int i = 1; i < L.length; i++)
+    {
+        if (L.data[i] < mini_element)
+        {
+            mini_element = L.data[i];
+            mini_index = i;
+        }
+    }
+
+    L.data[i] = L.data[L.length - 1];
+
+    L.length--;
+
+    // 以上这么做就行了, 如果纠结于浪费了一个空间的话, 可以用下面的语句
+    int *p = L.data;
+
+    L.data = (int *)malloc((L.length) * sizeof(int));
+
+    for (int i = 0; i < L.length; i++)
+    {
+        L.data[i] = *p[i];
+    }
+
+    free(p);
+    return false;
+}
+
---
+++ 第二章 线性表/综合练习题/2.2.3/2.cpp
@@ -0,0 +1,57 @@
+/* 题目:
+2. 设计一个高效算法, 将顺序表L的所有元素置逆, 要求算法的空间复杂度位O(1)
+*/
+
+#include <iostream>
+#include <string>
+
+#define MAXSIZE 10            // 定义最大长度
+
+typedef struct
+{
+    int data[MAXSIZE];
+    int length;
+}SqList;
+
+void InitList(Sqlist &L)
+{
+    for (int i = 0; i < L.length; i++)
+    {
+        L.data[i] = 0;
+    }
+
+    L.length = 0;
+}
+
+
+  /* 第一次写的时候的代码, 不符合空间复杂度的题意*/
+bool Sfun(Sqlist &L)
+{
+    int copy_data[MAXSIZE];
+
+    strcpy(copy_data, L.data);
+
+    for (int i = 0; i < L.length; i++)
+    {
+        L.data[i] = copy_data[L.length - 1 - i];
+    }
+
+    return true;
+}
+
+
+// 标准答案
+
+void Reverse(SqList &L)
+{
+    int temp;
+
+    for (int i = 0; i < L.length; i++)
+    {
+        temp = L.data[i];                       // 临时变量
+        L.data[i] = L.data[L.length - i - 1];
+        L.data[L.length - i - 1] = temp;
+    }
+}
+
+// 动态顺序表的操作与静态顺序表无异
