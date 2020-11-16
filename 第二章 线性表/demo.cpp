4ace906a1ef0e0cde81342760b84cd46a62e690e
Chunxian Zhang <1836891291@qq.com>
2020-11-16 14:02:08 +0800

    update Chapter 5 串

--- 第二章 线性表/demo.cpp
+++ 第二章 线性表/demo.cpp
@@ -1,16 +1,9 @@
-#include <stdio.h>
+#include <iostream>
+using namespace std;
 
 int main()
 {
-    int a = -1;
-
-    switch(a)
-    {
-        case -1:
-            printf("Success!");
-        default:
-        break;
-    }
-
-    return
+    int arr[3][4] = { {1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12} };
+    printf("")
 }
+
---
+++ 第四章 串/1串的顺序存储/1串结构的定义.cpp
@@ -0,0 +1,37 @@
+//? 头文件
+#include <stdio.h>
+
+
+//? 宏定义
+#define MAXSIZE 255                 // 预定义最大串长为255
+
+
+//? 串结构声明
+//串的定长顺序存储
+typedef struct
+{
+    char ch[MAXSIZE];               // 每个分量存储衣蛾字符
+    int length;                     // 串的实际长度
+}SString;
+
+// 数组的堆分配存储, 动态数组分配
+typedef struct
+{
+    char *ch;                       // 按串长分配存储区, ch指向串的基地址
+    int length;                     // 串的长度
+}HString;
+
+//! 串的链式存储
+// 每个节点存储一个字符, 缺点: 存储密度低
+typedef struct StringNode
+{
+    char ch;                        // 每个结点存一个字母
+    struct StringNode *next;
+}StringNode, * String;
+
+// 每个节点存储多个字符
+typedef struct StringNode
+{
+    char ch[4];
+    struct StingNode *next;
+}StringNode, *String;
