dda1f740cfe306b3945c3ab92019e823fe5d9928
Chunxian Zhang <1836891291@qq.com>
2020-12-21 15:27:05 +0800

    update SimpleSelectonSort.cpp

---
+++ 7第八章排序/5SimpleSelectionSort.cpp
@@ -0,0 +1,26 @@
+
+// 交换
+void swap(int &a, int &b) {
+    int temp = a;
+    a = b;
+    b = temp;
+}
+
+/**
+ * @brief 简单选择排序, 空间复杂度O(1), 时间复杂度O(n²)
+ * 算法不稳定
+ * @param A 
+ * @param n 
+ */
+void SelectSort(int A[], int n) {
+    for (int i = 0; i < n - 1; i++) {           // 一共进行n-1趟
+        int min = i;                            // min记录最小元素位置
+
+        for (int j = i + 1; j < n; j++) {       // 在A[i]到A[n-1]中选择最少的元素
+            if (A[j] < A[min]) min = j;         // 更新最小元素位置
+        }
+
+        if (min != i) swap(A[i], A[min]);       // 封装的swap函数共移动元素3次
+    }
+}
+
